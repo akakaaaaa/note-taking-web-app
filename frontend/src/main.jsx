d3559acfe426ae9d16f98f28009564b13418ec7a
akakaaaaa <akakitsipuria1@gmail.com>
2025-07-24 00:53:36 +0400

    test push

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,4 +9,4 @@ createRoot(document.getElementById("root")).render(
 );
 
 // 1. tags are changing design on click. for archive and home page i have to do separate notes, then i have to filter notes depending on specific tag
-// 2. create contexts
+// 2. create contextsss
